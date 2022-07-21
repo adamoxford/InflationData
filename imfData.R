ccf1cac62b6c50f52a3eeca36043312088d23a6e
Adam <adam@adamoxford.co.uk>
2022-07-21 10:30:21 +0200

    Updated ZAF data for June

--- imfData.R
+++ imfData.R
@@ -20,6 +20,8 @@ codeList <- read.csv("codeList.csv")
 
 kenyaData <- left_join(kenyaData, codeList, by = "Indicator.Name")
 
+southAfrica <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRuwJON5dg_MZ3ycdgaRFcrVnLBZjnRMGARcPnFZonvE1ug2vnY0cM3Hgh8zE_V9In2HQs9hFEAU6Ni/pub?gid=1551906459&single=true&output=csv")
+
 #get the latest country data from Google Sheet
 countryList <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vR6_bOjxa_F2qgn5eOGo6gpOLXKY9WcsebCvTPD4xVVs2yIL0ABtQx3QKhxfaftTI4jVj85mwaQQ0_K/pub?gid=0&single=true&output=csv")
 write_csv(countryList, "countryList.csv")
@@ -39,6 +41,8 @@ africaData <- countryList %>%
                     mutate(across(starts_with("X"), as.numeric)) %>%
                     mutate(across(starts_with("X"), round, 2)) %>%
                     full_join(kenyaData) %>%
+                    filter(Geography != "ZAF") %>%
+                    union(southAfrica) %>%
                     arrange(Country)
 
 names(africaData) <- sub("^X", "", names(africaData))

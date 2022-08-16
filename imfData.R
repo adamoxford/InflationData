b67ae81290d003e7abe0da931b6c06f7ea3fed64
Adam <adam@adamoxford.co.uk>
2022-08-16 11:59:09 +0200

    Updated to include latest Ethiopia data

--- imfData.R
+++ imfData.R
@@ -24,6 +24,8 @@ southAfrica <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRuwJON
 
 ugandaData <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRuwJON5dg_MZ3ycdgaRFcrVnLBZjnRMGARcPnFZonvE1ug2vnY0cM3Hgh8zE_V9In2HQs9hFEAU6Ni/pub?gid=1607745865&single=true&output=csv")
 
+ethiopiaData <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRuwJON5dg_MZ3ycdgaRFcrVnLBZjnRMGARcPnFZonvE1ug2vnY0cM3Hgh8zE_V9In2HQs9hFEAU6Ni/pub?gid=1693981224&single=true&output=csv")
+
 #get the latest country data from Google Sheet
 countryList <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vR6_bOjxa_F2qgn5eOGo6gpOLXKY9WcsebCvTPD4xVVs2yIL0ABtQx3QKhxfaftTI4jVj85mwaQQ0_K/pub?gid=0&single=true&output=csv")
 write_csv(countryList, "countryList.csv")
@@ -42,9 +44,10 @@ africaData <- countryList %>%
                     mutate(across(starts_with("X"), as.numeric)) %>%
                     mutate(across(starts_with("X"), round, 2)) %>%
                     full_join(kenyaData) %>%
-                    filter(Geography != "ZAF" & Geography != "UGA")%>%
+                    filter(Geography != "ZAF" & Geography != "UGA" & Geography != "ETH") %>%
                     full_join(southAfrica) %>%
                     full_join(ugandaData) %>%  
+                    full_join(ethiopiaData) %>%
                     arrange(Country) %>%
                     mutate(Indicator.Name = str_remove(Indicator.Name, ", Percentage change, Previous year"))
 
